a630afc8830e914c8f9bd568c583c28ec73aa857
rajeevmissc <rajeevm@issc.co.in>
2025-08-21 19:10:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,26 +2,29 @@ require('dotenv').config();
 const express = require('express');
 const cors = require("cors");
 const path = require("path");
-require('./config/modelConfig')
+require('./config/modelConfig');
 const mainRouter = require('./routes/mainRoute');
 const logger = require('./utils/logger');
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.use('/',mainRouter);
+app.use('/', mainRouter);
 
+// Run locally with app.listen
+if (!process.env.VERCEL) {
+    const HOST = process.env.HOST || "localhost";
+    const PORT = process.env.PORT || 8000;
+    const serverLink = `Server Started on http://${HOST}:${PORT}`;
 
-const HOST = process.env.HOST || "localhost" ;
-const PORT = process.env.PORT || 8000;
-const serverLink = `Server Started on http://${HOST}:${PORT}`
+    app.listen(PORT, () => {
+        console.log("Server is running on port:", PORT);
+        logger.log("info", serverLink);
+    });
+}
 
-app.listen(PORT,()=>{
-    console.log("Server is running on port: ",PORT)
-    logger.log("info",serverLink);
-
-})
-
-module.exports = app
+// Always export app for Vercel
+module.exports = app;
